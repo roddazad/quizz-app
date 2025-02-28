6fe9b63df7c0b8f490adab16304c7b042f6fb4f1
Rodd Azad <roddazad@Rodds-MacBook-Pro.local>
2025-02-27 16:29:48 -0800

    created hooks to the DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-button");
+const scoreDisplay = document.getElementById("score");
+
+let currentQuestionIndex = 0;
+let score = 0;
